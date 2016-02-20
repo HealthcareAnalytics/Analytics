73b46bdf1d7b77587f858e33341bdf6867fa5506
Brandon Tran <btran21@my.bcit.ca>
2016-02-19 20:43:35 -0800

    Create seed initializer

--- HealthcareAnalytics/HealthcareAnalytics/App_Start/Startup.Auth.cs
+++ HealthcareAnalytics/HealthcareAnalytics/App_Start/Startup.Auth.cs
@@ -15,7 +15,7 @@ namespace HealthcareAnalytics
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            Database.SetInitializer<HospitalDBContext>(new DropCreateDatabaseAlways<HospitalDBContext>());
+            Database.SetInitializer<HospitalDBContext>(new HospitalDBInitializer());
         }
     }
 }
