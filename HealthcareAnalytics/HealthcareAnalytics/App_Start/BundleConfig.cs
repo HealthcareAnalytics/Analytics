1fe0ea7966b5ac943217d0def126a88e0f5e0f1a
Sean-L <seanlavergne7@gmail.com>
2016-03-04 15:54:14 -0800

    Added print functionality to forms
    Signed-off-by: Sean-L <seanlavergne7@gmail.com>

--- HealthcareAnalytics/HealthcareAnalytics/App_Start/BundleConfig.cs
+++ HealthcareAnalytics/HealthcareAnalytics/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace HealthcareAnalytics
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Print").Include("~/Content/print.css"));
         }
     }
 }
